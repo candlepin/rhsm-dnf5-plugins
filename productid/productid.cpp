1836b2e7a471c6e44a10ebd56b7818223a3221f6
Jiri Hnidek <jhnidek@redhat.com>
2025-11-21 17:20:51 +0100

    feat: Implement downloading of productid certificates
    * Cleaned a code a little and deleted unused code from
      default template
    * Added new hooks:
      * repos_configured() directs "dnf" command to download
        productid certificates
      * post_transaction() list of downloaded productid
        certificates and setup filesystem a little.
    * Added documentation comments
    * Implemented own logging function to contain string
      '[productid plugin]' in every log message
    * Added lot of TODOs
    
    Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

--- productid/productid.cpp
+++ productid/productid.cpp
@@ -2,6 +2,7 @@
 #include <libdnf5/conf/const.hpp>
 #include <libdnf5/plugin/iplugin.hpp>
 
+#include <filesystem>
 #include <iostream>
 
 using namespace libdnf5;
@@ -18,6 +19,13 @@ constexpr const char * attrs_value[] {
     "jhnidek@redhat.com",
     "Automatically download productid certificates from Red Hat repositories."
 };
+
+
+#define PRODUCTDB_DIR "/var/lib/rhsm/"
+#define PRODUCTDB_FILE "/var/lib/rhsm/productid.json"
+
+constexpr const char * METADATA_TYPE_PRODUCTID = "productid";
+
 
 class ProductIdPlugin final : public plugin::IPlugin {
 public:
@@ -37,7 +45,7 @@ public:
     [[nodiscard]] const char * get_name() const noexcept override { return PLUGIN_NAME; }
 
     /// Fill in the version of your plugin.
-    /// This is utilized in informative and debugging log messages.
+    /// This is used in informative and debugging log messages.
     /// MANDATORY to override.
     [[nodiscard]] plugin::Version get_version() const noexcept override { return PLUGIN_VERSION; }
 
@@ -54,36 +62,138 @@ public:
         }
         return nullptr;
     }
-
-    /// Initialization method called after the Base object is created and before command-line arguments are parsed.
-    /// Optional to override.
-    void init() override {}
-
-    /// Cleanup method called when plugin objects are garbage collected.
-    /// Optional to override.
-    void finish() noexcept override {}
-
-    /// Override the hooks you want to implement.
-    void post_base_setup() override {
+    
+    void repos_configured() override {
+        repos_configured_hook();
     }
-    void pre_transaction(const base::Transaction & transaction) override {
-        pre_transaction_magic(transaction);
+
+    void post_transaction(const base::Transaction & transaction) override {
+        post_transaction_hook(transaction);
     };
 
     ConfigParser & config;
 
 private:
-    void pre_transaction_magic(const base::Transaction &) const;
+    // Own logging
+    template <typename... Ss>
+    void debug_log(std::string_view format, Ss &&... args) const;
+
+    template <typename... Ss>
+    void warning_log(std::string_view format, Ss &&... args) const;
+
+    template <typename... Ss>
+    void error_log(std::string_view format, Ss &&... args) const;
+
+    // Hooks
+    void repos_configured_hook() const;
+    void post_transaction_hook(const base::Transaction &) const;
+
+    [[nodiscard]] bool setup_filesystem() const ;
 };
 
-/// Example how to implement additional logic before starting the transaction.
-void ProductIdPlugin::pre_transaction_magic(const base::Transaction & transaction) const {
+template <typename... Ss>
+void ProductIdPlugin::debug_log(std::string_view format, Ss &&... args) const {
+    Base & base = get_base();
+    base.get_logger()->debug("[productid plugin] " + libdnf5::utils::sformat(format, std::forward<Ss>(args)...));
+}
+
+template <typename... Ss>
+void ProductIdPlugin::warning_log(std::string_view format, Ss &&... args) const {
+    Base & base = get_base();
+    base.get_logger()->warning("[productid plugin] " + libdnf5::utils::sformat(format, std::forward<Ss>(args)...));
+}
+
+template <typename... Ss>
+void ProductIdPlugin::error_log(std::string_view format, Ss &&... args) const {
+    Base & base = get_base();
+    base.get_logger()->error("[productid plugin] " + libdnf5::utils::sformat(format, std::forward<Ss>(args)...));
+}
+
+/// This method is called before transaction processing starts, and we do here final
+/// configuration adjustments. We order the dnf to also try to download productid metadata.
+void ProductIdPlugin::repos_configured_hook() const {
+    Base & base = get_base();
+    debug_log("repos_configured hook started");
+    debug_log("Order dnf to download additional metadata type: productid");
+    base.get_config().get_optional_metadata_types_option().set(METADATA_TYPE_PRODUCTID);
+    debug_log("repos_configured hook finished");
+}
+    
+/// The management of productid certificates is triggered here after the transaction is finished.
+/// It covers two use cases:
+///
+/// 1. Try to get downloaded product certificates when the new package is installed, and the
+///    product certificate has not been installed yet. Install this product certificate
+///    to /etc/pki/product if needed. Add the RPM repository to our "database" in
+///    /var/lib/rhsm/productid.json file
+/// 2. When the RPM package was removed, then if there is any repository in the
+///    /var/lib/rhsm/productid.json file, then remove it from the "database". When
+///    the productid database contains productid with no RPM repository, then
+///    remove this productid from the database and then remove the productid certificate
+///    from /etc/pki/product directory.
+void ProductIdPlugin::post_transaction_hook(const base::Transaction & transaction) const {
     (void)transaction;
-    if (config.has_option("main", "enabled")) {
-        Base & base = get_base();
-        base.get_logger()->debug("Productid plugin enabled");
-        std::cout << "Productid plugin enabled" << std::endl;
+    Base & base = get_base();
+
+    debug_log("post_transaction hook started");
+
+    // First, try to create all necessary directories
+    if (!setup_filesystem()) return;
+
+    // Get the list of enabled repositories
+    repo::RepoQuery repos(base);
+    repos.filter_enabled(true);
+
+    // When there are no enabled repositories, then we cannot try to get
+    // any product certificate from anywhere, and we can finish this hook here.
+    if (repos.empty()) {
+        warning_log("No enabled repositories found; Exiting.");
+        return;
     }
+
+    // Go through all enabled repositories and try to get paths of downloaded productid certificates.
+    // Note: when the transaction is e.g. "remove", then cached metadata is empty, but we will probably
+    //       not need cached metadata during removal of packages.
+    for (const auto & repo : repos) {
+        std::string productid_path = repo->get_metadata_path(METADATA_TYPE_PRODUCTID);
+        if (!productid_path.empty()) {
+            debug_log(
+                "productid certificates of '{}' repository downloaded to: {}",
+                repo->get_id(),
+                productid_path
+                );
+        } else {
+            debug_log(
+                "'{}' does not contain productid certificates",
+                repo->get_id()
+                );
+        }
+        // TODO: When productid_path is not empty, then try to install it to the system, when at least
+        //       one RPM package is installed from this repository
+    }
+
+    // TODO: Try to remove installed productid certificates when needed
+
+    debug_log("post_transaction hook finished");
+}
+
+/// This plugin needs the existence of several directories. Try to create these directories.
+bool ProductIdPlugin::setup_filesystem() const {
+    // Try to create the directory where we store product certificates "database"
+    if (std::filesystem::exists(PRODUCTDB_DIR)) {
+        debug_log("Directory {} already exists", PRODUCTDB_DIR);
+    } else {
+        debug_log("Directory {} does not exist, creating it", PRODUCTDB_DIR);
+        try {
+            std::filesystem::create_directories(PRODUCTDB_DIR);
+        } catch (const std::filesystem::filesystem_error &e) {
+            error_log(
+                "Failed to create directory {}: {}; Exiting", PRODUCTDB_DIR, e.what());
+            return false;
+        }
+    }
+    // TODO: Create other directories when needed
+    return true;
 }
 
 }  // namespace
@@ -91,17 +201,17 @@ void ProductIdPlugin::pre_transaction_magic(const base::Transaction & transactio
 /// Below is a block of functions with C linkage used for loading the plugin binaries from disk.
 /// All of these are MANDATORY to implement.
 
-/// Return the plugin's API version.
+/// Return the plugin API version.
 PluginAPIVersion libdnf_plugin_get_api_version(void) {
     return REQUIRED_PLUGIN_API_VERSION;
 }
 
-/// Return the plugin's name.
+/// Return the plugin name.
 const char * libdnf_plugin_get_name(void) {
     return PLUGIN_NAME;
 }
 
-/// Return plugin's version.
+/// Return the plugin version.
 plugin::Version libdnf_plugin_get_version(void) {
     return PLUGIN_VERSION;
 }
